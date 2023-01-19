e9d0c514c14c5812ad21ea730d5515a99c033ecc
angelo562 <username@example.com>
2023-01-18 16:58:20 -0800

    Added to_csv, using from&to clipboard

---
+++ Project_blackout/project_blackout.py
@@ -0,0 +1,138 @@
+"""
+Takes in an excel file with group shift stats on a particular month
+Takes as input another excel file as a request graph
+For each provider matching provider, blacksout each date onto another excel file along with an indicator text marking 'w' for work concatenated with extra text if shift ends at 8pm or later
+"""
+
+import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+import os
+from datetime import datetime
+
+def main():
+
+    inp1 = input(f"Please input location path of group shift stats: ").strip()
+    PATH_group_stats = fr'{inp1[1:-1]}'
+
+    inp2 = input(f"Please input location path of request graph: ").strip()
+    PATH_req_graph = fr'{inp2[1:-1]}'
+
+    PATH_request_processed1 = os.path.abspath(r"C:\Users\Angel\anaconda3\envs\pandas_processed1.xlsx")
+
+
+    process_shift_data(PATH_group_stats, PATH_req_graph)
+    fill_cells(PATH_req_graph, PATH_request_processed1)
+
+    print(f"SUCCESS! Saved as : group_assigned_shifts_and_requests.xlsx ")
+
+def process_shift_data(PATH_group_stats, PATH_req_graph):
+    # reads the shift excel file given as an argument and creates a dataframe
+
+    # reads excel with header
+    df = pd.read_excel(PATH_group_stats, header =1)
+
+    #adds f_name, l_name, & end_time
+    name_data = df['Provider'].str.split(" ", n=1, expand=True)
+    df['f_name'] = name_data[0]
+    df['l_name'] = name_data[1]
+    time_data = df['Time'].str.split(" - ", n=1, expand=True)
+    df['end_time'] = time_data[1] + 'm'
+
+    #changes dtype obj->datetime
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['end_time'] = pd.to_datetime(df['end_time'], format='%I%p')
+
+    # drop rows/Providers without shifts where df.Shift = NaN
+    df = df[df['Shift'].notna()]
+    df = df.reset_index(drop=True)
+
+    process_request_data(df, PATH_req_graph)
+
+
+def process_request_data(df, PATH_req_graph):
+    
+    df_r = pd.read_excel(PATH_req_graph, header =1)
+
+    # delete On / off / time of columns and then reset index.
+    # ONLY has providers with scheduled shifts  
+    df_r = df_r.drop(df_r.tail(4).index)
+    df_r.drop(columns=['On','Off','Time Of'])
+
+    # for each name in df, add shifts from df to df_request (df_r)
+    for i, row in df.iterrows():
+        name = row['Provider']
+        day = row['Date'].day
+        end_time = row['end_time']
+
+        # TODO create a datetime object to simplify below
+
+        late_times = ['20:00:00', '21:00:00', '22:00:00', '23:00:00', '00:00:00','01:00:00', '02:00:00', '06:00:00']
+
+        
+        # if provider is in df_r, otherwise add
+        if name not in df_r.User.tolist():
+
+            # If Provider in df doesn't exist in df_r, create row in df with 'User':name
+            new_row = pd.DataFrame({'User': name}, index=[-1])
+            df_r2 = pd.concat([df_r.loc[:], new_row]).reset_index(drop=True)
+            df_r = df_r2.copy()
+
+        # find index of provider in df
+        provider_index = df_r.User[df_r.User == name ].index.tolist()[0]
+
+        # write onto the cell with an indicator
+        indicator = 'w'
+        if str(end_time.time()) in late_times:
+            indicator += ' til' + str(end_time.time())[:5]
+        df_r.at[provider_index, str(day)] = indicator
+
+    # save excel file 
+    df_r.to_excel('pandas_processed1.xlsx')
+
+
+# TODO add prior month day columns ~3 days +/- shade 
+
+# TODO For each provider, for each shift, color code cells Black or next cell Red if df.end_time is 8p or after.
+
+def fill_cells(PATH_req_graph, PATH_request_processed1):
+    wba = load_workbook(PATH_req_graph, data_only = True)
+    wba.save(filename='temp.xslx')
+    sh = wba['Request Graph']
+
+
+    wb_r = load_workbook(PATH_request_processed1, data_only=True)
+    wb_r.save(filename = 'temp2.xslx')
+    sh_r = wb_r['Sheet1']
+
+    # get  dict(hex_data) for each provider for each cell
+    provider_hex_d = dict(zip(
+        [row[0] for row in sh.iter_rows(min_row=3, max_row=(sh.max_row - 4), max_col=(1), values_only=True)], 
+
+        [[cell.fill.start_color.index  for cell in row] for row in sh.iter_rows(min_col=2, max_col=(sh.max_column-4), min_row=3, max_row=(sh.max_row - 4))]
+    )) 
+
+    color_d = {'10': '00C0C0C0' ,'11':'00808080', '12':'00b200','9': 'ff8b94', '00000000': '00FFFFFF' }
+
+    for row in sh_r.iter_rows(min_col=2, max_col=(sh_r.max_column - 1), min_row=2, max_row=(sh_r.max_row)):
+        key = row[0].value
+
+        try:
+
+            for i in range(len(provider_hex_d[key])):
+                #fill cell  and move to next cell
+                cellc_to_fill = row[i + 1].coordinate
+
+                code = str(provider_hex_d[key][i])
+                sh_r[cellc_to_fill].fill = PatternFill(fgColor=color_d[code], fill_type = "solid")
+
+        except KeyError:
+            pass
+    
+    now = datetime.now()
+    s = f'{now.month:02d}_{now.day}_{now.hour}{now.minute}'
+    PATH_export = os.path.abspath(fr'C:\Users\Angel\Downloads\{s}group_assigned_shifts_and_requests.xlsx')
+    wb_r.save(filename = PATH_export)
+
+if __name__ == "__main__":
+    main()
---
+++ Project_xlsx_to_csv/to_csv.py
@@ -0,0 +1,186 @@
+"""
+Will take a selection or excel, with row index as FULL provider names, and columns as the shift name in the form F.N, F.D, or F.D-F.N and output a csv file with row of shifts on the left with each provider's shift admin shorthand name in each cell for the designated day.
+
+Template file should be local...downloaded each time?
+"""
+import pandas as pd
+import os
+import numpy as np
+
+
+def main():
+    prompt = input(f"Copy your selection. Make sure to include row of days.  Press enter when ready. Or enter path of excel file: ")
+
+    df = None
+    if not prompt:
+        print(f"going selection")
+        df = read_selection()
+    
+    else:
+        prompt = prompt.strip()[1:-1]
+        formats = ('.xls','xlsx')
+        if prompt.endswith(formats):
+            df = read_excel_to_df(prompt)
+        else:
+            print(f"file error")
+
+    process(df)
+
+    print(f"Done, it is NOW SAVED TO YOUR CLIPBOARD.  Paste starting with 'shift'")
+
+
+def read_selection():
+    return pd.read_clipboard()
+    
+def read_excel_to_df(PATH_1):
+    return pd.read_excel(PATH_1, index_col=0)
+
+def path_to_csv_template():
+    prompt = input(f"Please enter path to this month's csv template: ")
+    prompt = prompt.strip()[1:-1]
+    return os.path.abspath(fr"{prompt}")
+
+
+def process(df):
+
+    shift_names = ["F.D", "F.N", "B.D", "B.N", "S.D", "S.N", "C.D", "C.N"]
+
+    csv_code = {
+        "F.D": "SRDHWF F.D",
+        "F.N": "SRDHWF F.N",
+        "B.D": "SRDHBD B.D",
+        "B.N": "SRDHBD B.N",
+        "S.D": "SRDHS S.D",
+        "S.N": "SRDHS S.N",
+        "C.D": "SRDHNL C.D",
+        "C.N": "SRDHNL C.N",
+        None: None,
+    }
+
+    provider_sadmin_names = {
+        "Christopher Cheng": "C. Cheng",
+        "Waseya Cornell": "W. Cornell",
+        "Jeffery Gardner": "J. Gardner",
+        "Scott Ferguson": "S. Ferguson",
+        "Caleb Anderson": "C. Anderson",
+        "Peter Fuller": "P. Fuller",
+        "Shilpa Gupta": "S. Gupta",
+        "Michael Hixson": "M. Hixson",
+        "Stephen Hunt": "S. Hunt",
+        "Rocky Jedick": "R. Jedick",
+        "Jason Kim": "J. Kim",
+        "Angelo Kim": "A. Kim",
+        "Tim Kuo": "T. Kuo",
+        "Nicholas Leaver": "N. Leaver",
+        "Jaclyn Matsuura": "J. Matsuura",
+        "Cameron Macadams": "C. Macadams",
+        "Roger Martinez": "R. Martinez",
+        "Ashkan Morim": "A. Morim",
+        "Quin Newby": "Q. Newby",
+        "David Notley": "D. Notley",
+        "Stephanos Orphanidis": "S. Orphanidis",
+        "Daniel Pendleton": "D. Pendleton",
+        "Kevin Slaughter": "K. Slaughter",
+        "Irena Vitkovitsky": "I. Vitkovitsky",
+        "Franklin Alconcel": "F. Alconcel",
+        "Lian Farino": "L. Farino",
+        "Nelson Huang": "N. Huang",
+        "John Jobes": "J. Jobes",
+        "Joseph Kim": "J.Y. Kim",
+        "Jason Klein": "J. Klein",
+        "Michael Koroscil": "M. Koroscil",
+        "Eduardo Lacalle": "E. Lacalle",
+        "Norlan Maltez": "N. Maltez",
+        "Patrick Noone": "P. Noone",
+        "Krystal Ribeiro": "K. Ribeiro",
+        "Anne Sinnott": "A. Sinnott",
+        "Michael Tang": "M. Tang",
+        "Sabrina Taylor": "S. Taylor",
+        "Joseph Tran": "Jo. Tran",
+        "Kyle Vanstone": "K. Vanstone",
+        "Harold Woo": "H. Woo",
+        "Lance Allgower": "L. Allgower",
+        "Samuel Bergin": "S. Bergin",
+        "Vincent Boyd": "V. Boyd",
+        "Jefferson Bracey": "J. Bracey",
+        "Kenneth Chang": "K. Chang",
+        "Elizabeth Chen": "E. Chen",
+        "Todd Christensen": "T. Christensen",
+        "Patrick Flaherty": "P. Flaherty",
+        "Daniel Gowhari": "D. Gowhari",
+        "William Halacoglu": "W. Halacoglu",
+        "Brett Hansen": "B. Hansen",
+        "Bryce Hoer": "B. Hoer",
+        "Hayden Maag": "H. Maag",
+        "Dennis Martinez": "D. Martinez",
+        "Grace Sasaki": "G. Sasaki",
+        "Andrew Sheep": "A. Sheep",
+        "Kathryn Sulkowski": "K. Sulkowski",
+        "Jesse Tran": "J. Tran",
+        "Shuai Zhao": "S. Zhao",
+        "Donald Bennett": "D. Bennett",
+        "Adam Berkovits": "A. Berkovits",
+        "Frederick Griffith": "F. Griffith",
+        "Joshua Imakyure": "J. Imakyure",
+        "Daniel Inglish": "D. Inglish",
+        "Jason Jones": "J.P. Jones",
+        "Juan Jones": "J.C. Jones",
+        "Patricia Kahn": "P. Kahn",
+        "Eugene Kang": "E. Kang",
+        "John Kim": "J. Kim",
+        "Brandi Landis": "B. Landis",
+        "Joseph Lo": "J. Lo",
+        "Peter Malamet": "P. Malamet",
+        "Heber Phillips": "H. Phillips",
+        "Raanan Pokroy": "R. Pokroy",
+        "Juan Reynoso": "J. Reynoso",
+        "Schon Roberts": "S. Roberts",
+        "Jesse Wells": "J. Wells",
+    }
+
+    
+    df = df.replace(r"^\s*$", np.nan, regex=True)
+    df["provider"] = df.index
+    df.provider = df["provider"].map(lambda x: provider_sadmin_names[x])
+    df = df.set_index("provider")
+
+    # lets create a dictionary with User and names of shift
+    d_psd = {}
+    for row in df.iterrows():
+        k, v = row[0], row[1].tolist()
+        d_psd[k] = v
+
+    # modifies the list to only include shift names or None
+    for k, v in d_psd.items():
+        temp_v = []
+        for elem in v:
+            if elem in shift_names or ("-" in str(elem)):
+                temp_v.append(elem)
+            else:
+                temp_v.append(None)
+        d_psd[k] = temp_v
+
+    # read csv template data
+    PATH_2 = path_to_csv_template()
+    df_csv = pd.read_csv(PATH_2, header=9, index_col=1)
+
+    for provider, list_shifts in d_psd.items():
+        for day, p_shift in enumerate(list_shifts, start=1):
+            if p_shift:
+
+                if "-" in p_shift:
+                    p_shift1, p_shift2 = tuple(p_shift.split("-"))
+                    df_csv.at[csv_code[p_shift1], str(day)] = provider
+                    df_csv.at[csv_code[p_shift2], str(day)] = provider
+
+                else:
+                    df_csv.at[csv_code[p_shift], str(day)] = provider
+
+    df_csv = df_csv.drop(columns=['ID'])
+
+    df_csv.to_clipboard()
+
+
+if __name__ == "__main__":
+    main()
+
